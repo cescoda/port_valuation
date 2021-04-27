840d9c34cf94ed4c7cf669bd9a316b189e7db5fb
Cristina Escoda <cescoda@users.noreply.github.com>
2021-04-27 09:19:44 -0700

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -12,11 +12,12 @@ def funding_round(cap_table_array, round_data_array):
     others_shares_i = cap_table_array[1]
     tachyon_shares_i = cap_table_array[2]
     ESOP_shares_i = cap_table_array[3]
-    pre_money_amount = round_data_array[0]
-    round_amount = round_data_array[1]
-    tachyon_amount = round_data_array[2]
+    pre_money_amount = round_data_array[0]*1000000
+    round_amount = round_data_array[1]*1000000
+    tachyon_amount = round_data_array[2]*1000000
     ESOP = round_data_array[3]
-
+    print ("ESOP")
+    print (round_data_array[3])
     
     total_shares_i = tachyon_shares_i + founders_shares_i + others_shares_i
     
@@ -32,16 +33,43 @@ def funding_round(cap_table_array, round_data_array):
     dilution = round_amount / (pre_money_amount+round_amount)
     
     
+    print ("founders_shares_i, others_shares_i, tachyon_shares_i, ESOP_shares_i")
+    print (cap_table_array)
+    print ("round data array initial")
+    print (round_data_array)
+    
     cap_table_array = np.array([founders_shares_f, others_shares_f, tachyon_shares_f, ESOP_shares_f],dtype='i')
     price_array = np.array([pps, dilution]) 
+    
+    
+    
+    print ("founders_shares_f, others_shares_f, tachyon_shares_f, ESOP_shares_f")
     print (cap_table_array)
+    print ("pps, dilution")
     print (price_array)
     
     return np.array([cap_table_array, price_array])
     #cap_table_array, round_data_array
- 
+    
+    
+    
+Prob_Matrix = np.array([[0.7,0.2,0.1],[0.7,0.2,0.1],[0.7,0.2,0.1],[0.7,0.2,0.1],[0.7,0.2,0.1],[0.7,0.2,0.1]])
+
+
+Round_data_matrix = np.array([[[12,5,0,0.1],[5,2,0,0.1],[2,0.5,0,0.1]],#seed - high, medium, low
+                              [[25,15,0,0.1],[15,8,0,0.1],[8,4,0,0.1]],#A
+                              [[75,25,0,0.1],[40,20,0,0.1],[25,8,0,0.1]],#B
+                              [[250,75,0,0.1],[120,30,0,0.1],[75,25,0,0.1]],#C
+                              [[500,150,0,0.1],[250,75,0,0.1],[150,75,0,0.1]],#D
+                              [[1000,0,0,0.1],[500,0,0,0.1],[250,0,0,0.1]]])#exit
+
+print ("round data matrix")
+print (Round_data_matrix[0][0])#Seed - high
+print (Round_data_matrix[0][1])#Seed - medium
+
+print ("Seed Round")
 cap_table_array = np.array([1000000,0,0,0],dtype='i') #founders_shares_f, others_shares_f, tachyon_shares_f, ESOP_shares_f
-Seed_round_data_array = np.array([8000000,4000000,150000,0.1]) #pre_money_amount, round_amount, tachyon_amount_i, ESOP
+Seed_round_data_array = Round_data_matrix[0][0] #pre_money_amount, round_amount, tachyon_amount_i, ESOP
 Seed_array = funding_round(cap_table_array,Seed_round_data_array)
 print ("cap table: founders_shares_f, others_shares_f, tachyon_shares_f, ESOP_shares_f")
 print (Seed_array[0])
@@ -49,7 +77,7 @@ print ("price data: PPS, dilution")
 print (Seed_array[1])
 
 print ("Series A")
-SeriesA_round_data_array = np.array([20000000,10000000,0,0.1]) #pre_money_amount, round_amount, tachyon_amount_i, ESOP
+SeriesA_round_data_array = Round_data_matrix[1][0] #pre_money_amount, round_amount, tachyon_amount_i, ESOP
 SeriesA_array = funding_round(Seed_array[0],SeriesA_round_data_array)
 print ("cap table: founders_shares_f, others_shares_f, tachyon_shares_f, ESOP_shares_f")
 print (SeriesA_array[0])
@@ -57,7 +85,7 @@ print ("price data: PPS, dilution")
 print (SeriesA_array[1])
 
 print ("Series B")
-SeriesB_round_data_array = np.array([75000000,20000000,0,0.1]) #pre_money_amount, round_amount, tachyon_amount_i, ESOP
+SeriesB_round_data_array = Round_data_matrix[2][0] #pre_money_amount, round_amount, tachyon_amount_i, ESOP
 SeriesB_array = funding_round(SeriesA_array[0],SeriesB_round_data_array)
 print ("cap table: founders_shares_f, others_shares_f, tachyon_shares_f, ESOP_shares_f")
 print (SeriesB_array[0])
@@ -65,7 +93,7 @@ print ("price data: PPS, dilution")
 print (SeriesB_array[1])
 
 print ("Series C")
-SeriesC_round_data_array = np.array([150000000,5000000,0,0.1]) #pre_money_amount, round_amount, tachyon_amount_i, ESOP
+SeriesC_round_data_array = Round_data_matrix[3][0] #pre_money_amount, round_amount, tachyon_amount_i, ESOP
 SeriesC_array = funding_round(SeriesB_array[0],SeriesC_round_data_array)
 print ("cap table: founders_shares_f, others_shares_f, tachyon_shares_f, ESOP_shares_f")
 print (SeriesC_array[0])
@@ -73,7 +101,7 @@ print ("price data: PPS, dilution")
 print (SeriesC_array[1])
 
 print ("Series D")
-SeriesD_round_data_array = np.array([250000000,7500000,0,0.1]) #pre_money_amount, round_amount, tachyon_amount_i, ESOP
+SeriesD_round_data_array = Round_data_matrix[4][0] #pre_money_amount, round_amount, tachyon_amount_i, ESOP
 SeriesD_array = funding_round(SeriesC_array[0],SeriesD_round_data_array)
 print ("cap table: founders_shares_f, others_shares_f, tachyon_shares_f, ESOP_shares_f")
 print (SeriesD_array[0])
@@ -84,76 +112,5 @@ print (SeriesD_array[1])
 #High
 #Medium
 #Low
-
-Prob_Matrix = np.array([0.7,0.2,0.1],[0.7,0.2,0.1],[0.7,0.2,0.1],[0.7,0.2,0.1],[0.7,0.2,0.1],[0.7,0.2,0.1])
-
-
-Round_data_matrix = np.array([[12000000,5000000,0,0.1],[5000000,2000000,0,0.1],[2000000,500000,0,0.1]],#seed - high, medium, low
-                              [[25000000,15000000,0,0.1],[15000000,8000000,0,0.1],[8000000,4000000,0,0.1]],#A
-                              [[75000000,25000000,0,0.1],[40000000,2000000,0,0.1],[25000000,750000,0,0.1]],#B
-                              [[250000000,7500000,0,0.1],[120000000,30000000,0,0.1],[75000000,25000000,0,0.1]],#C
-                              [[5000000000,150000000,,0,0.1],[250000000,7500000,0,0.1],[150000000,7500000,0,0.1]],#D
-                              [[1000000000,0,0,0.1],[500000000,0,0,0.1],[250000000,0,0,0.1]])#exit
-
-
-#Seed Round 
-stage = "Seed Round"
-founders_shares_i=1000000
-others_shares_i=0
-tachyon_shares_i=0
-ESOP_shares_i=0
-pre_money_amount=8000000
-round_amount=4000000
-tachyon_amount=150000
-ESOP=0.1
-
-Seed_Round = (founders_shares_i,others_shares_i,tachyon_shares_i,ESOP_shares_i,pre_money_amount,round_amount, tachyon_amount, ESOP)
-print (Seed_Round)
-#A_Round = funding_round(Seed_Round)
-
-    
-founders_shares_i,others_shares_i,tachyon_shares_i,ESOP_shares_i,total_shares_i,pps,dilution = funding_round(founders_shares_i,others_shares_i,tachyon_shares_i,ESOP_shares_i,pre_money_amount,round_amount,tachyon_amount,ESOP)
-print ("Stage=", stage)
-print ("founders_shares=", founders_shares_i)
-print ("others_shares=", others_shares_i)
-print ("tachyon_shares=", tachyon_shares_i)
-print ("ESOP_shares=", ESOP_shares_i)
-print ("total_shares=", total_shares_i)
-print ("pps=", pps)
-print ("dilution=", dilution)
-print ("-----")
-
-#Series A 
-stage = "Series A"
-pre_money_amount=25000000
-round_amount=10000000
-
-founders_shares_i,others_shares_i,tachyon_shares_i,ESOP_shares_i,total_shares_i,pps,dilution = funding_round(founders_shares_i,others_shares_i,tachyon_shares_i,ESOP_shares_i,pre_money_amount,round_amount,tachyon_amount,ESOP)
-print ("Stage=", stage)
-print ("founders_shares=", founders_shares_i)
-print ("others_shares=", others_shares_i)
-print ("tachyon_shares=", tachyon_shares_i)
-print ("ESOP_shares=", ESOP_shares_i)
-print ("total_shares=", total_shares_i)
-print ("pps=", pps)
-print ("dilution=", dilution)
-print ("-----")
-
-#Series B 
-stage = "Series B"
-pre_money_amount=50000000
-round_amount=20000000
-
-founders_shares_i,others_shares_i,tachyon_shares_i,ESOP_shares_i,total_shares_i,pps,dilution = funding_round(founders_shares_i,others_shares_i,tachyon_shares_i,ESOP_shares_i,pre_money_amount,round_amount,tachyon_amount,ESOP)
-
-print ("Stage=", stage)
-print ("founders_shares=", founders_shares_i)
-print ("others_shares=", others_shares_i)
-print ("tachyon_shares=", tachyon_shares_i)
-print ("ESOP_shares=", ESOP_shares_i)
-print ("total_shares=", total_shares_i)
-print ("pps=", pps)
-print ("dilution=", dilution)
-print ("-----")
 
 
