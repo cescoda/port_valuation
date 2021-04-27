66074f1f54080fe9a639fcd8b23bc890c6135be1
Cristina Escoda <cescoda@users.noreply.github.com>
2021-04-26 17:27:32 -0700

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -3,46 +3,46 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from scipy import integrate
 
-def funding_round(funding_array):
+def funding_round(cap_table_array, round_data_array):
     #Provide round data
-    #founders_shares_i, others_shares_i, tachyon_shares_i, ESOP_shares_i, pre_money_amount, round_amount, tachyon_amount, ESOP
-    founders_shares_i =funding_array[0]
-    others_shares_i=funding_array[1]
-    tachyon_shares_i=funding_array[2]
-    ESOP_shares_i=funding_array[3]
-    pre_money_amount=funding_array[4]
-    round_amount=funding_array[5]
-    tachyon_amount=funding_array[6]
-    ESOP=funding_array[7]
+    #founders_shares_i, others_shares_i, tachyon_shares_i, ESOP_shares_i, 
+    #pre_money_amount, round_amount, tachyon_amount, ESOP
+    
+    founders_shares_i = cap_table_array[0]
+    others_shares_i = cap_table_array[1]
+    tachyon_shares_i = cap_table_array[2]
+    ESOP_shares_i = cap_table_array[3]
+    pre_money_amount = round_data_array[0]
+    round_amount = round_data_array[1]
+    tachyon_amount = round_data_array[2]
+    ESOP = round_data_array[3]
 
     
     total_shares_i = tachyon_shares_i + founders_shares_i + others_shares_i
-    others_amount = round_amount - tachyon_amount
-    pps = pre_money_amount/total_shares_i
-    tachyon_shares_f = tachyon_amount/pps+tachyon_shares_i
-    post_money_amount = pre_money_amount+round_amount
-    others_shares_f = others_amount/pps+others_shares_i
+    
     founders_shares_f = founders_shares_i
+    others_shares_f = others_amount/pps+others_shares_i
+    tachyon_shares_f = tachyon_amount/pps+tachyon_shares_i
     total_shares_f = (tachyon_shares_f + founders_shares_f + others_shares_f)/(1-ESOP)
     ESOP_shares_f = ESOP*total_shares_f+ESOP_shares_i
+    
+    others_amount = round_amount - tachyon_amount
+    post_money_amount = pre_money_amount+round_amount
+    pps = pre_money_amount/total_shares_i
     dilution = round_amount / (pre_money_amount+round_amount)
     
-    funding_ar= np.array([founders_shares_f, others_shares_f, tachyon_shares_f, ESOP_shares_f]) 
-    funding_ar_2=np.array([pps, dilution])
-    funding_round_ar = np.array([funding_ar, funding_ar_2])
-    print (funding_round_ar[0])
     
-    return funding_round_ar
+    cap_table_array = np.array([founders_shares_f, others_shares_f, tachyon_shares_f, ESOP_shares_f])
+    price_array = np.array(pps, dilution]) 
+    print (cap_table_array)
+    print (price_array)
+    
+    #return np.array([cap_table_array, round_data_array])
+    cap_table_array, round_data_array
  
- 
-cap_table = np.array([1000000,0,0,0])
-funding_data = np.array([8000000000,4000000,150000,0.1])
-funding_array = np.concatenate((cap_table,funding_data))
-#funding_round(funding_array)
-print (funding_array[0])
-funding_round_Seed=funding_round(funding_array)
-
-funding_data_seriesA=
+cap_table_array = np.array([1000000,0,0,0]) #founders_shares_f, others_shares_f, tachyon_shares_f, ESOP_shares_f
+round_data_array = np.array([8000000000,4000000,150000,0.1]) #pre_money_amount, round_amount, tachyon_amount_i, ESOP
+cap_table_array_Seed, round_data_array_Seed = funding_round(cap_table_array,round_data_array)
 
 
 
